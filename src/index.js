23058642bbdb0abcb969a101ce9db58bd003c9e6
ar00gula <arianna.mayer@gmail.com>
2021-01-08 15:33:07 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.getElementById("submit-button").addEventListener('click', function(event) {
+    event.preventDefault();
+    //prevent default
+    let li = document.createElement('li');
+    let deleteButton = document.createElement("input");
+        deleteButton.type = 'button';
+        deleteButton.value = 'x';
+        deleteButton.id = 'delete-button';
+        //create delete button and li
+
+    let task = document.getElementById("new-task-description");
+    li.innerHTML = task.value;
+    li.id = "list-item";
+    document.getElementById("tasks").appendChild(li).appendChild(deleteButton);
+    task.value = "";
+
+    deleteButton.addEventListener('click', function() {
+      deleteButton.parentElement.remove();
+    });
+  });
+  
 });
+
+
+
